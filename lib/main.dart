6670df6c1893d5bc8baf4658863759a3c0e9bf16
YujiOnishi <49971039+YujiOnishi@users.noreply.github.com>
2021-10-21 00:53:55 +0900

    fix_design
    ・デザインの微修正
    　画像の変更（境界線がわかりにくいため）
    　ダークモードに変更（テキストとの差異を少なくするため）
    　記事の表示範囲を変更（講義の都合上）

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Atomic Design',
-      theme: ThemeData.light(),
+      theme: ThemeData.dark(),
       home: const Index(),
     );
   }
