cf08d2672dbee1f9ab9a740474dd2925325b0318
jiyuujin <jiyuujinunite@gmail.com>
2021-11-03 17:38:25 +0900

    feat: docs

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,128 @@
+export default {
+  title: `FlutterKaigi 2021 workshop | ドキュメント一覧`,
+  description: `ハンズオンでご紹介のあった資料を中心に記録しています。`,
+  head: [
+    [
+      `meta`,
+      {
+        hid: `description`,
+        name: `description`,
+        content: `FlutterKaigi 2021 ハンズオンでご紹介のあった資料を中心に記録しています。`
+      }
+    ],
+    [
+      `meta`,
+      {
+        hid: `title`,
+        name: `title`,
+        content: `FlutterKaigi 2021 workshop | ドキュメント一覧`
+      }
+    ],
+    [
+      `meta`,
+      {
+        hid: `og:type`,
+        name: `og:type`,
+        content: `website`
+      }
+    ],
+    [
+      `meta`,
+      {
+        hid: `og:description`,
+        name: `og:description`,
+        content: `FlutterKaigi 2021 ハンズオンでご紹介のあった資料を中心に記録しています。`
+      }
+    ],
+    [
+      `meta`,
+      {
+        hid: `og:title`,
+        name: `og:title`,
+        content: `FlutterKaigi 2021 workshop | ドキュメント一覧`
+      }
+    ],
+    [
+      `meta`,
+      {
+        hid: `og:url`,
+        name: `og:url`,
+        content: `https://flutterkaigi-2021-workshop.netlify.app`
+      }
+    ],
+    [
+      `meta`,
+      {
+        hid: `og:image`,
+        name: `og:image`,
+        content: ``
+      }
+    ],
+    [
+      `meta`,
+      {
+        hid: `twitter:description`,
+        name: `twitter:description`,
+        content: `FlutterKaigi 2021 ハンズオンでご紹介のあった資料を中心に記録しています。`
+      }
+    ],
+    [
+      `meta`,
+      {
+        hid: `twitter:title`,
+        name: `twitter:title`,
+        content: `FlutterKaigi 2021 workshop | ドキュメント一覧`
+      }
+    ],
+    [
+      `meta`,
+      {
+        hid: `twitter:card`,
+        name: `twitter:card`,
+        content: `summary` // ex: summary, summary_large_image
+      }
+    ],
+    // [
+    //   `meta`,
+    //   {
+    //     hid: `twitter:site`,
+    //     name: `twitter:site`,
+    //     content: ``
+    //   }
+    // ],
+    // [
+    //   `meta`,
+    //   {
+    //     hid: `twitter:creator`,
+    //     name: `twitter:creator`,
+    //     content: ``
+    //   }
+    // ],
+    // [
+    //   `meta`,
+    //   {
+    //     hid: `twitter:site`,
+    //     name: `twitter:site`,
+    //     content: ``
+    //   }
+    // ],
+    // [
+    //   `meta`,
+    //   {
+    //     hid: `twitter:image`,
+    //     name: `twitter:image`,
+    //     content: ``
+    //   }
+    // ],
+  ],
+  base: `/`,
+  themeConfig: {
+    repo: `FlutterKaigi/atomic_design_handson`,
+    editLinks: false,
+    docsDir: `docs`,
+    nav: [
+      //
+    ],
+    sidebarDepth: 3,
+  }
+}
---
+++ docs/index.md
@@ -0,0 +1,21 @@
+# FlutterKaigi workshop
+
+## ハンズオン概要
+
+記念すべき [FlutterKaigi](https://flutterkaigi.jp/) 1 回目のハンズオンでは Atomic Design を扱う。
+
+![](https://i.imgur.com/pBCwqQv.jpg)
+
+### 進め方
+
+::: tip 事前に参加者が構築します。
+
+2021/11/30 (火) 開催予定の [FlutterKaigi ハンズオン](https://flutterkaigi.connpass.com/event/227769/) にご参加される皆さまには [事前準備](https://github.com/FlutterKaigi/atomic_design_handson/wiki) を済ませていただくようよろしくお願いいたします。
+
+イベント開始直前の時点で HelloWorld アプリとして作成されるカウントアップアプリが出来上がったら OK です。もし不明な点がおありの方は [FlutterKaigi/atomic_design_handson](https://github.com/FlutterKaigi/atomic_design_handson) を参考にしていただければ幸いです。
+
+:::
+
+## 参照
+
+最終的に参加者は [FlutterKaigi/_atomic_design_handson](https://github.com/FlutterKaigi/_atomic_design_handson) (後日公開予定) を目指していただく。
