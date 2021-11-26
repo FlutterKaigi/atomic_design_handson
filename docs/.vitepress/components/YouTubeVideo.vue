3cffde338b8f99a5633851d4908ff4d8b252d415
jiyuujin <jiyuujinunite@gmail.com>
2021-11-26 19:14:45 +0900

    feat: setup - embed youtube

---
+++ docs/.vitepress/components/YouTubeVideo.vue
@@ -0,0 +1,23 @@
+<template>
+  <LiteYouTubeEmbed :id="videoId" :title="title" />
+</template>
+
+<script lang="ts">
+import LiteYouTubeEmbed from 'vue-lite-youtube-embed'
+import 'vue-lite-youtube-embed/dist/style.css'
+export default {
+  components: {
+    LiteYouTubeEmbed
+  },
+  props: {
+    videoId: {
+      type: String,
+      default: ''
+    },
+    title: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,4 +1,10 @@
 import DefaultTheme from 'vitepress/theme'
+import YouTubeVideo from '../components/YouTubeVideo.vue'
 import './custom.css'
 
-export default DefaultTheme
+export default {
+  ...DefaultTheme,
+  enhanceApp({ app }) {
+    app.component('YouTubeVideo', YouTubeVideo)
+  }
+}
